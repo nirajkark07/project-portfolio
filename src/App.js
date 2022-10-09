fb30b8cdde2bbaf6c96eba12e6a4f9da03347a5f
Niraj Karki <nirajkarki@live.com>
2022-10-09 17:58:20 -0400

    Initializing react project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { Button } from '@mui/material';
+
+function App() {
+  return (
+    <div className="App">
+      <Button>tets</Button>
+    </div>
+  );
+}
+
+export default App;
